28fd8811b3e1447da6bc985bef672f0f6d255096
David Silva <david_goncalvessilva@hotmail.com>
2024-03-03 23:23:57 +0000

    ngx-translate installation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'ng-portfolio';
+export class AppComponent implements OnInit {
+  title = 'Portfolio';
+
+  constructor(private translateService: TranslateService) { }
+
+  ngOnInit(): void {
+    this.translateService.setDefaultLang('pt');
+  }
 }
